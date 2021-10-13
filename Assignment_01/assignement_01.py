add6ebdda9772b21e48e2c9833fa0643ca16c343
emil-ho <81522556+emil-ho@users.noreply.github.com>
2021-10-13 18:59:38 +0200

    Update assignement_01.py
    now working

--- Assignment_01/assignement_01.py
+++ Assignment_01/assignement_01.py
@@ -1,2 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# importing the parameter file
+try:
+    with open('data.txt') as f:
+        lines = f.readlines()
+except FileNotFoundError:
+    print("The file was not found")
+
+# creating a dictonary with the parameters
+params = {}
+for param in lines:
+    splitedS = param.split()
+    params[splitedS[0]] = splitedS[1]
+
+# generating random numbers
+np.random.seed(12424)
+
+if params["distributionType"] == 'g':
+    numbers = float(params["widthSDFraction"]) * np.random.randn(int(params["totalNTries"]))
+else:
+    print(f"{params['distributionType']} is an unknown distribution type")
+    
+# computing the histogram
+hist, bin_edges = np.histogram(numbers, bins=int(params["nBins"]))
+
+# plotting the histogram
+plt.hist(bin_edges[:-1], bin_edges, weights=hist)
+plt.show()
