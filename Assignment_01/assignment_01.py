fed04480edd8718071207d74beff028acb281d18
Emil Hoffmann <emil.hoffmann99@gmail.com>
2021-10-16 15:42:32 +0200

    added test + info

--- Assignment_01/assignment_01.py
+++ Assignment_01/assignment_01.py
@@ -11,6 +11,21 @@ for param in lines:
     splitedS = param.split()
     params[splitedS[0]] = splitedS[1]
 
+# checking if all important values are provided
+if 'distributionType' not in params:
+    print('The parameter "distributionType" is missing')
+elif 'nBins' not in params:
+    print('The parameter "nBins" is missing')
+elif 'totalNTries' not in params:
+    print('The parameter "totalNTries" is missing')
+elif 'distributionType' == 'g':
+    if 'widthSDFraction' not in params:
+        print('The parameter "widthSDFraction" is missing')
+    else:
+        pass
+else:
+    pass
+
 # generating random numbers and creating the histogram
 np.random.seed(12424)
 
@@ -31,5 +46,5 @@ plt.hist(bin_edges[:-1], bin_edges, weights=hist)
 plt.xlabel("value")
 plt.ylabel("occurence")
 plt.title("random value distribution")
-plt.savefig('histogram.jpg')
+plt.text(bin_edges[0],0.8 * max(hist),f"distributionType: {params['distributionType']} \nnBins: {params['nBins']} \ntotalNTries: {params['totalNTries']}", fontsize=8)
 plt.show()
